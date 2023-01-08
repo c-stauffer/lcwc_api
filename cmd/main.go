1a3d5b32906da2e14c21b8e2936ecc6a9b8ff76f
chris Stauffer <cstauffer@outlook.com>
2023-01-08 09:41:11 -0500

    more code

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 
+	"code.crogge.rs/chris/lcwc_api/pkg/handlers"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/books", func(w http.ResponseWriter, r *http.Request) {
-		json.NewEncoder(w).Encode("Hello World")
-	})
+	//router.HandleFunc("/books", func(w http.ResponseWriter, r *http.Request) {
+	//	json.NewEncoder(w).Encode("Hello World")
+	//})
+	router.HandleFunc("/books", handlers.GetAllBooks).Methods(http.MethodGet)
 
 	log.Println("API is running!")
 	http.ListenAndServe(":4000", router)
---
+++ pkg/mocks/book.go
@@ -0,0 +1,12 @@
+package mocks
+
+import "code.crogge.rs/chris/lcwc_api/pkg/models"
+
+var Books = []models.Book{
+	{
+		Id:     1,
+		Title:  "Golang",
+		Author: "Gopher",
+		Desc:   "A book for Go",
+	},
+}
